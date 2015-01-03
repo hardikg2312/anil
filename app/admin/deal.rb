1f8fa1df3c5a8654245f6576dd33e459d9b8becd
hardikg23 <hardikg23@gmail.com>
2015-01-03 13:03:38 +0530

    deal admin

--- app/admin/deal.rb
+++ app/admin/deal.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Deal do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :title, :category_id, :deal_url, :description, :valid_till, :deal_valid, :discount_price, :price, :photo_url
   #
   # or
   #
@@ -13,6 +13,41 @@ ActiveAdmin.register Deal do
   #   permitted << :other if resource.something?
   #   permitted
   # end
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs "Details" do
+      f.input :title
+      f.input :category_id, :as => :select, :collection => Category.all.map { |c| [ c.name, c.id ] }
+      f.input :deal_url
+      f.input :description
+      f.input :valid_till
+      f.input :deal_valid
+      f.input :discount_price
+      f.input :price
+      #f.input :category_photo, :as => :file, :hint => f.template.image_tag(f.object.category_photo.url)
+    end
+    f.actions
+  end
+
+  controller do
+    def show
+      @deal = Deal.friendly.find_by_slug(params[:id])
+    end
+
+    def edit
+      @deal = Deal.friendly.find_by_slug(params[:id])
+    end
+
+    def update
+      @deal = Deal.friendly.find_by_slug(params[:id])
+      super
+    end
+
+    def destroy
+      @deal = Deal.friendly.find_by_slug(params[:id])
+      super
+    end
+  end
 
 
 end
--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,11 @@
 class Deal < ActiveRecord::Base
 
   belongs_to :category, :counter_cache => true
+
+  # validations
+  validates_presence_of   :title, :deal_url
+  validates_uniqueness_of :slug    # uniq also at database level
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
