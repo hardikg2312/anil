d3d6f91596b226c34aa35f903c7c9c4fd7c5874d
hardikg23 <hardikg23@gmail.com>
2015-01-03 19:37:13 +0530

    Deal index page

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,6 +1,9 @@
 class DealsController < ApplicationController
 
-	def index
-		render :text => "hello"
-	end
+  def index
+    @categories = Category.all
+    @category = Category.friendly.find_by_slug(params[:catogory]) if params[:catogory].present? && params[:catogory] != 'all'
+    @deals = @category.deals.where(:deal_valid => true).order('id desc') if @category
+    @deals ||= Deal.includes(:category).where(:deal_valid => true).order('id desc')
+  end
 end
